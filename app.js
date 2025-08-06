2c8e820fda7a62257721db8a8a653a9015e20032
Kevin Araujo <kevinaraujoo677@gmail.com>
2025-08-06 15:46:09 -0300

    Função para exibir lista no HTML

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let amigos = [];
-
+let i = 0;
 function adicionarAmigo(){
     nome = document.getElementById('amigo').value;
 
@@ -9,4 +9,15 @@ function adicionarAmigo(){
     
     amigos.push(nome);
     document.getElementById('amigo').value = '';
+
+    exibirAmigos(amigos);
+}
+
+function exibirAmigos(amigos){
+    lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for(let i = 0; i < amigos.length; i++){
+        document.getElementById('listaAmigos').innerHTML += `<li>${amigos[i]}</li>`;
+    }
 }
